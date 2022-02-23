0186575367adb33b66ef82b31d6ac67735ab0f15
Leandro Correia <leandro.correia@serpro.gov.br>
2022-02-23 11:36:38 -0300

    Only Integers Challenge

---
+++ only_ints.py
@@ -0,0 +1,2 @@
+def is_only_ints(p1, p2):
+    return isinstance(p1, int) and isinstance(p2, int)
---
+++ only_ints_test.py
@@ -0,0 +1,10 @@
+import unittest
+from only_ints import is_only_ints
+
+class TestOnlyInts(unittest.TestCase):
+
+    def test_is_only_ints(self):
+        self.assertTrue(is_only_ints(1, 2))
+        self.assertFalse(is_only_ints(1, 'b'))
+        self.assertFalse(is_only_ints('a', 2))
+        self.assertFalse(is_only_ints('a', 'b'))
