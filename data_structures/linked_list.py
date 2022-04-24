2ea4f38c3344b2406e394f5dac0ec9dc2ef30bf4
Leandro Correia <leandro.correia@serpro.gov.br>
2022-04-24 18:10:29 -0300

    Linked List pop

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -9,20 +9,46 @@ class LinkedList:
     def __init__(self, value):
         self.head = Node(value)
         self.tail = self.head
+        self.length = 1
 
     def append(self, value):
         new_node = Node(value)
         self.tail.next = new_node
         self.tail = new_node
+        self.length+=1
 
     def prepend(self, value):
         new_node = Node(value)
         new_node.next = self.head
         self.head = new_node
+        self.length+=1
+
+    def pop(self):
+        pointer_node = self.lookup(self.length-1)
+        pop_node = pointer_node.next
+        pointer_node.next = None
+        self.tail = pointer_node
+        self.length-=1
+        
+        return pop_node
+
+    def lookup(self, index):
+        if index > self.length - 1:
+            index = self.length - 1
+        
+        count=0
+        pointer_node = self.head
+
+        while count<(index-1):
+            pointer_node = pointer_node.next
+            count+=1
+
+        return pointer_node
 
     def __str__(self):
         
-        printed_list = "\nHEAD: "+str(self.head.value)
+        printed_list = "\nSIZE: "+str(self.length)
+        printed_list += "\nHEAD: "+str(self.head.value)
         printed_list += "\nTAIL: "+str(self.tail.value)
         printed_list += "\n"
 
@@ -35,6 +61,8 @@ class LinkedList:
 
 l = LinkedList("Google")
 l.append("Udemy")
-print(l)
 l.prepend("Discord")
 print(l)
+print(l.pop().value)
+print(l)
+
