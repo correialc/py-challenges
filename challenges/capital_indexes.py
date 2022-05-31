4786b602f5f26a5f1dbc27b8016946530fb770fd
Leandro Correia <leandro.correia@serpro.gov.br>
2022-05-31 08:58:08 -0300

    Updated solution for Capital Indexes challenge

--- challenges/capital_indexes.py
+++ challenges/capital_indexes.py
@@ -1,2 +1,10 @@
-def find_capital_indexes(one_string):
-    return [idx for idx, value in enumerate(one_string) if value.isupper()]
+'''
+CAPITAL LETTERS
+Given a string, return a list of all the indexes in the string 
+that have capital letters. 
+'''
+
+from typing import List
+
+def find_capital_indexes(text: str) -> List:
+    return [i for i, v in enumerate(text) if v.isupper()]
--- tests/challenges_tests/capital_indexes_test.py
+++ tests/challenges_tests/capital_indexes_test.py
@@ -5,3 +5,6 @@ class TestCapitalIndexes(unittest.TestCase):
 
         def test_find_capital_indexes(self):
                 self.assertEqual(find_capital_indexes("HellO!"), [0, 4])
+                self.assertEqual(find_capital_indexes("H"), [0])
+                self.assertEqual(find_capital_indexes("i"), [])
+                self.assertEqual(find_capital_indexes(""), [])
