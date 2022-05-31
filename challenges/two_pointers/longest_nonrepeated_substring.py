5162d681ee1f7e846ac09dd7e7459941413ec359
Leandro Correia <leandro.correia@serpro.gov.br>
2022-05-31 09:38:11 -0300

    WIP: Longest Nonrepeated Substring Challenge

---
+++ challenges/two_pointers/longest_nonrepeated_substring.py
@@ -0,0 +1,21 @@
+def find_longest_substring(text: str) -> int:
+    left = 0
+    right = 0
+    longest = 0
+    sub = ""
+    sub_size = 0
+
+    while right<len(text):
+        if text[right] in sub:
+            while text[left] != text[right]:
+                left+=1
+            
+            left+=1
+            sub = text[left:right + 1]
+            sub_size = len(sub)
+        else:
+            sub += text[right]
+            longest = max(longest, len(sub))
+            right += 1 
+
+    return longest
---
+++ tests/challenges_tests/longest_nonrepeated_substring_test.py
@@ -0,0 +1,12 @@
+import unittest
+from challenges.two_pointers.longest_nonrepeated_substring import find_longest_substring
+
+class TestLongestNonRepeatedSubstring(unittest.TestCase):
+
+    def test_find_longest_substring(self):
+        self.assertEqual(find_longest_substring('abcdbea'), 5)
+        self.assertEqual(find_longest_substring('aba'), 2)
+        self.assertEqual(find_longest_substring('abccabcabcc'), 3)
+        self.assertEqual(find_longest_substring('aaaabaaa'), 2)
+
+
