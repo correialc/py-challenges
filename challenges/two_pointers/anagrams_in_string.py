ae71eee154b60e3153cbd90ea4dc84a49dd78e0c
Leandro Correia <leandro.correia@serpro.gov.br>
2022-06-02 20:21:30 -0300

    Anagrams in String challenge

--- challenges/two_pointers/anagrams_in_string.py
+++ challenges/two_pointers/anagrams_in_string.py
@@ -11,20 +11,22 @@ Constraints:
 - 1 <= len(original), len(check) <= 10^5
 - Each string consists of only lowercase characters in standard English alphabet.
 '''
-
+from collections import Counter
 from typing import List
 
 def find_anagrams_in_string(text: str, check: str) -> List:
     
     left = 0
     right = len(check)
+    check = Counter(check)
+    window = Counter(text[left:right])
     indexes = []
 
     while right<=len(text):
-        if ''.join(sorted(text[left:right])) == check:
+        if window == check:
             indexes.append(left)
         
         left += 1
         right += 1
-    
+        window = Counter(text[left:right])
     return indexes
