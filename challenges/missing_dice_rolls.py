3065c4a25bf0214dda416be1f9a4a756a0c6f1b8
Leandro Correia <leandro.correia@gmail.com>
2022-08-10 10:42:19 -0300

    Missing Dice Rolls challenge

---
+++ challenges/missing_dice_rolls.py
@@ -0,0 +1,12 @@
+from itertools import combinations_with_replacement
+
+def solution(A, F, M):
+
+    dice_rolls_perm = combinations_with_replacement([1,2,3,4,5,6], F) 
+    missing_sum = M * (len(A) + F) - sum(A)
+
+    for p in list(dice_rolls_perm):
+        if sum(p) == missing_sum:
+            return list(p)
+    
+    return [0]
