433d2cde8b7ffb44bd0450a963ecd1c3fe5bfc3c
Leandro Correia <leandro.correia@gmail.com>
2022-11-19 17:03:38 -0300

    FIX: Remove inefficient list instantiation

--- challenges/missing_dice_rolls.py
+++ challenges/missing_dice_rolls.py
@@ -6,7 +6,7 @@ def missing_dice_rolls(A, F, M):
     dice_rolls_perm = combinations_with_replacement([1, 2, 3, 4, 5, 6], F)
     missing_sum = M * (len(A) + F) - sum(A)
 
-    for p in list(dice_rolls_perm):
+    for p in dice_rolls_perm:
         if sum(p) == missing_sum:
             return list(p)
 
