f813558bea5ef7f4bf97df3e739628d15057cef9
Leandro Correia <leandro.correia@gmail.com>
2022-11-19 17:00:40 -0300

    FIX: Missing Dice Rolls function name

--- challenges/missing_dice_rolls.py
+++ challenges/missing_dice_rolls.py
@@ -1,12 +1,13 @@
 from itertools import combinations_with_replacement
 
-def solution(A, F, M):
 
-    dice_rolls_perm = combinations_with_replacement([1,2,3,4,5,6], F) 
+def missing_dice_rolls(A, F, M):
+
+    dice_rolls_perm = combinations_with_replacement([1, 2, 3, 4, 5, 6], F)
     missing_sum = M * (len(A) + F) - sum(A)
 
     for p in list(dice_rolls_perm):
         if sum(p) == missing_sum:
             return list(p)
-    
+
     return [0]
